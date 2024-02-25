4cf16df68ec6270fad796dc6d78e0352cce03980
gottanned <gottanned@gmail.com>
2024-02-24 23:24:30 -0600

    modified:   index.css 	modified:   numbergame.html 	modified:   numbergame.js

--- numbergame.js
+++ numbergame.js
@@ -18,6 +18,7 @@ function resetGame() {
   document.getElementById("attemptedCounter").textContent = "";
   document.getElementById("guessList").innerHTML = "";
   document.getElementById("resetButton").style.display = "none";
+  document.getElementById("dragInstruction").textContent = "Drag to reset game";
 }
 
 window.onload = function () {
@@ -27,9 +28,10 @@ window.onload = function () {
 
 function checkGuess() {
   guess = document.getElementById("guessInput").value;
-  console.log(guess);
   let feedBack;
   let resultBackground;
+  let inputField = document.getElementById("guessInput");
+  let correctGuess = false;
 
   if (guess < 1 || guess > 100) {
     feedBack = "Please enter a number between 1 and 100";
@@ -37,6 +39,7 @@ function checkGuess() {
   } else if (guess == secretNumber) {
     feedBack =
       "Congratulations! You guessed the number! Refresh the page to play again!";
+    correctGuess = true;
     resultBackground = "#93C572";
   } else if (guess < secretNumber) {
     feedBack = "Too low! Try again!";
@@ -48,11 +51,32 @@ function checkGuess() {
   document.getElementById("attemptedCounter").textContent =
     "Attempted #" + ++noGuess;
 
-  document.getElementById("resetButton").style.display = "block";
+  document.getElementById("resetButton").style.display = "";
 
-  document.getElementById("feedbackArea").textContent = feedBack;
-  document.getElementById("feedbackArea").style.backgroundColor =
-    resultBackground;
+  if (correctGuess === false) {
+    var audio = new Audio("http://soundbible.com/grab.php?id=1540&type=wav");
+    audio.play();
+    inputField.classList.add("shake-animation");
+
+    setTimeout(function () {
+      inputField.classList.remove("shake-animation");
+    }, 1000);
+  } else {
+    var audio = new Audio(
+      "https://www.myinstants.com/media/sounds/grindr-notification-sound.mp3"
+    );
+    audio.play();
+  }
+
+  let feedbackArea = document.querySelector("#feedbackArea");
+  //feedbackArea.style.backgroundColor = resultBackground;
+  if (feedbackArea.classList.contains("feedback-animation")) {
+    feedbackArea.classList.remove("feedback-animation");
+    void feedbackArea.offsetWidth; // Trigger reflow to restart the animation
+  }
+  feedbackArea.classList.add("feedback-animation");
+
+  feedbackArea.textContent = feedBack;
 
   let li = document.createElement("li");
   li.textContent = "Attempt #" + noGuess + " - " + guess + " : " + feedBack;
@@ -61,6 +85,108 @@ function checkGuess() {
 
 const guessButton = document.getElementById("guessButton");
 guessButton.addEventListener("click", checkGuess);
+guessButton.addEventListener(
+  "mouseover",
+  function (e) {
+    guessButton.style.borderColor = "black";
+
+    setTimeout(function () {
+      guessButton.style.borderColor = "#89CFF0";
+    }, 500);
+  },
+  false
+);
+guessButton.addEventListener(
+  "mouseout",
+  function (e) {
+    guessButton.style.borderColor = "white";
+
+    setTimeout(function () {
+      guessButton.style.borderColor = "#89CFF0";
+    }, 500);
+  },
+  false
+);
+
+const guessInput = document.getElementById("guessInput");
+guessInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    checkGuess();
+  }
+});
+
+guessInput.addEventListener(
+  "animationend",
+  function () {
+    guessInput.focus();
+  },
+  false
+);
 
 const resetButton = document.getElementById("resetButton");
 resetButton.addEventListener("click", resetGame);
+resetButton.addEventListener(
+  "mouseover",
+  function (e) {
+    resetButton.style.borderColor = "white";
+
+    setTimeout(function () {
+      resetButton.style.borderColor = "#D22B2B";
+    }, 500);
+  },
+  false
+);
+resetButton.addEventListener(
+  "mouseout",
+  function (e) {
+    resetButton.style.borderColor = "black";
+
+    setTimeout(function () {
+      resetButton.style.borderColor = "#D22B2B";
+    }, 500);
+  },
+  false
+);
+
+function dragstartHandler(e) {
+  const dragInstruction = document.getElementById("dragInstruction");
+  dragInstruction.textContent =
+    "Drag the icon to the drop zone or release the mouse to dismiss";
+  e.dataTransfer.setData("text/plain", e.target.id);
+  e.dataTransfer.dropEffect = "move";
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const draggedItem = document.getElementById("draggableIcon");
+  draggedItem.addEventListener(
+    "dragstart",
+    (e) => {
+      dragstartHandler(e);
+    },
+    false
+  );
+});
+
+function dragoverHandler(e) {
+  e.preventDefault();
+  const dragInstruction = document.getElementById("dragInstruction");
+  dragInstruction.textContent = "Release the mouse to drop the icon";
+  e.dataTransfer.dropEffect = "move";
+}
+
+function dropHandler(e) {
+  e.preventDefault();
+  const id = e.dataTransfer.getData("text/plain");
+  const dropZone = document.getElementById("droppableIcon");
+  const draggedItem = document.getElementById(id);
+
+  dragInstruction.textContent = "Icon dropped successfully! Reset game now ...";
+  setTimeout(function () {
+    resetGame();
+  }, 500);
+}
+
+function dragendHandler(e) {
+  const dragInstruction = document.getElementById("dragInstruction");
+  dragInstruction.textContent = "Drag to reset game";
+}
